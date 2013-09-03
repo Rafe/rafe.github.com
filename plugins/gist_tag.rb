64f5447247510ebb5d4418511f9029e7e60c9f9e
Jimmy Chao <daizenga@gmail.com>
2013-09-03 00:37:14 -0400

    fix gist

--- plugins/gist_tag.rb
+++ plugins/gist_tag.rb
@@ -35,7 +35,6 @@ module Jekyll
       code = CGI.escapeHTML code
       <<-HTML
 <div><script src='#{script_url}'></script>
-<script type="text/javascript">$(".gist-file table tr td.line-numbers").remove();</script>
 <noscript><pre><code>#{code}</code></pre></noscript></div>
       HTML
     end
